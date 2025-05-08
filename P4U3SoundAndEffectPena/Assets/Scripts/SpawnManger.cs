2a55c3f16adde462e815b8c3ad3ad7ee755e841c
mstyyyyy <722833@student.dusd.net>
2025-05-08 11:27:30 -0700

    Finished
    Finished Project

--- P4U3SoundAndEffectPena/Assets/Scripts/SpawnManger.cs
+++ P4U3SoundAndEffectPena/Assets/Scripts/SpawnManger.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class SpawnManger : MonoBehaviour
 {
     public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
     private PlayerController playerControllerScript;
+    private int randomObstacle;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +28,8 @@ public class SpawnManger : MonoBehaviour
     {
         if(playerControllerScript.gameOver == false)
         {
-            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
         }
 
     }
